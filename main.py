04b193f1b46582a8daba988165870e6a31bcbc78
CormacCollins <100655400@student.swin.edu.au>
2018-07-06 11:24:37 +1000

    Early working gp template populator, needs input patient info to document - then future GUI work

---
+++ main.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+from mailmerge import MailMerge
+from datetime import date
+import csv
+
+# populates fields -- needs to change if columns in csv change
+def populateDocMerge(document, row):
+        document.merge(
+            doctor_full_name=row[1], # 0 is the title - maybe change later
+            doctor_surgery=row[2],
+            address_1=row[3],
+            address_2=row[4],
+            doctor_surname=row[5]        
+        )
+
+
+
+# return list of array rows (GP details)
+def getGPList(fileName):
+    gpList = list()
+    with open(fileName, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        # print("successfully opened" + str(fileName)    
+        
+        for row in reader:
+            gpList.append(row)
+
+    return gpList
+
+def findGP(name, gpList):
+    for row in gpList:
+        print(row)
+        if(row[2] == name):
+            return row
+
+    return None
+
+# ------------------------- Main ----------------------------
+
+gpListFile = 'addresses.csv'
+template = "gp_template.docx"
+document = MailMerge(template)
+print("Fields:")
+fields_set = document.get_merge_fields()
+print(fields_set)
+
+gpName = input()
+
+gpList = getGPList(gpListFile)
+gpInfo = findGP(gpName, gpList)
+
+if gpInfo is not None:
+    populateDocMerge(document, gpInfo)
+    try:
+        document.write('test-output.docx')
+    
+    except:
+        print("Unable to open file")
+else:
+    print("Could not find specified GP info.")
+
+
+
+
+
+
+
+
